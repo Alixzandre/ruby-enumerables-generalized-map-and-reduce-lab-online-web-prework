ca261779ff0cf105d111f4e5e0ddf4a85043227b
Alexander Bispo <alexbispo173@gmail.com>
2019-09-04 01:33:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,10 +2,10 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push(source_array[i])
+    new.push(yield(source_array[i]))
     i += 1
   end
   new
-  yield (source_array[i])
+  
 end
  
