a9cb41e90ab14c1acb1471bcbbda2c5a01395b17
Alexander Bispo <alexbispo173@gmail.com>
2019-09-04 01:45:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def reduce(source_array,sv=0)
     i = 1
   end
   while i < source_array.length do
-    new.push(yield(source_array[i]))
+    sum = yield(source_array[i])
     i += 1
   end
   sum
